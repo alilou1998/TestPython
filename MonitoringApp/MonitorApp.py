473b99a40bda4bc2339870ed9080b65b06c8267f
alilou1998 <alimalaoui3@gmail.com>
2020-03-18 16:36:15 +0100

    commit

--- MonitoringApp/MonitorApp.py
+++ MonitoringApp/MonitorApp.py
@@ -12,7 +12,4 @@ print("La valeur finale de x : ",x)
 ############
 print("La valeur finale de racinecarre(x) : ",sqrt(A))
 print("Le nombre d'iteration : ",log2(526))
-print("Mac")
-print("Xps")
 print("Xps2")
-print("Mac2")
